011fabf6a2ccdbf13229af14f77626987db0db0c
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-04-17 22:24:24 +0000

    tidy up startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,12 +1,10 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y software-properties-common
+sudo apt install -y software-properties-common gcc-riscv64-linux-gnu
 
 rustup default nightly
 rustup target add riscv64gc-unknown-none-elf 
 cargo install cargo-binutils 
-
-sudo apt-get install -y gcc-riscv64-linux-gnu
 
 if [ ! -d "qemu" ]; then
     sudo bash "scripts/install-qemu.sh"
