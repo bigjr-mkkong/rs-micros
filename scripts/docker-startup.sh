621672181667d1e79759fbc18c9defc7ed27fa9a
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-04-18 20:37:27 +0000

    Formatter

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -5,6 +5,7 @@ sudo apt install -y software-properties-common gcc-riscv64-linux-gnu
 rustup default nightly
 rustup target add riscv64gc-unknown-none-elf 
 cargo install cargo-binutils 
+rustup component add rustfmt
 
 if [ ! -d "qemu" ]; then
     sudo bash "scripts/install-qemu.sh"
